ea0927e5af693df7185d06c117b0601d76ff41ec
william <wmchan622@gmail.com>
2020-12-08 23:51:47 +1100

    finish 2015 day 4

--- 2015/days/04.ts
+++ 2015/days/04.ts
@@ -1,15 +1,28 @@
 #!/usr/bin/env ts-node-script
 
+const md5 = require('md5');
 import get_input from '../utils/getline';
 
 let input: string = get_input();
 
+const findHashStartingWith = (inp: string, num: string): number  => {
+    let i: number = 0;
+    let hash: string = inp + i;
+
+    while (!md5(hash).startsWith(num)) {
+        ++i;
+        hash = inp + i;
+    }
+
+    return i;
+}
+
 const p1 = (inp: string): number => {
-    return 0;
+    return findHashStartingWith(inp, '00000');
 }
 
 const p2 = (inp: string): number => {
-    return 0;
+    return findHashStartingWith(inp, '000000');
 }
 
 console.log(p1(input));
--- 2015/utils/getline.ts
+++ 2015/utils/getline.ts
@@ -4,12 +4,15 @@ const fs = require('fs');
 const readline = require('readline');
 
 const get_input = (): string => {
+    let inp;
     if (process.argv.length != 3) {
-        return fs.readFileSync(0, 'utf8');
+        inp = fs.readFileSync(0, 'utf8');
     }
     else {
-        return fs.readFileSync(process.argv[2], 'utf8');
+        inp = fs.readFileSync(process.argv[2], 'utf8');
     }
+
+    return inp.split(/\r?\n/).filter(line => line !== '').map(line => line.trim());
 }
 
 export default get_input;
