1f829e4c6e4249c16754f63d8cca3f1a34262082
william <wmchan622@gmail.com>
2020-12-14 19:25:39 +1100

    finish day11 pt2

--- 2020/days/11.py
+++ 2020/days/11.py
@@ -48,140 +48,63 @@ def check_adjacent(seats, i, j):
     occupied = [True if x == '#' else False for x in adj_list]
     return occupied 
 
-def check_first_seen(seats, i, j):
-    adj_list = []
-    # north west
-    x, y = i, j
-    while x >= 0 and y >= 0:
-        if (x == i and y == j) or seats[x][y] == '.':
-            x -= 1
-            y -= 1
-            continue
-        if seats[x][y] != '.':
-            adj_list.append(seats[x][y])
-            break
-        x -= 1
-        y -= 1
+def bfs(seats, start, direction):
+    q = [start]
+    seen = set()
 
-    # north
-    x, y = i, j
-    while x >= 0:
-        if x == i or seats[x][y] == '.':
-            x -= 1
-            continue
-        if seats[x][y] != '.':
-            adj_list.append(seats[x][y])
-            break
+    while q:
+        curr = q.pop(0)
+        x, y = curr
 
-        x -= 1
-    
-    # north east
-    x, y = i, j
-    while x >= 0 and y < len(seats[0]):
-        if (x == i and y == j) or seats[x][y] == '.':
-            x -= 1
-            y += 1
-            continue
-        if seats[x][y] != '.':
-            adj_list.append(seats[x][y])
-            break
-        x -= 1
-        y += 1
+        if seats[x][y] != '.' and curr != start:
+            return x, y 
+        
+        seen.add(curr)
 
-    # north east
-    x, y = i, j
-    while x >= 0 and y < len(seats[0]):
-        if (x == i and y == j) or seats[x][y] == '.':
-            x -= 1
-            y += 1
-            continue
-        if seats[x][y] != '.':
-            adj_list.append(seats[x][y])
-            break
-        x -= 1
-        y += 1
+        x_dir, y_dir = direction
+        nb = (x + x_dir, y + y_dir)
+        if 0 <= x + x_dir < len(seats) and 0 <= y + y_dir < len(seats[0]):
+            if nb not in seen:
+                q.append(nb)
+    return None
 
-    # west
-    x, y = i, j
-    while y >= 0:
-        if (x == i and y == j) or seats[x][y] == '.':
-            y -= 1
-            continue
-        if seats[x][y] != '.':
-            adj_list.append(seats[x][y])
-            break
-        y -= 1
+        
+def get_neighbours(seats, start):
+    neighbours = []
+    neighbours.append(bfs(seats, start, (-1, -1))) # NW
+    neighbours.append(bfs(seats, start, (-1, 0)))  # N
+    neighbours.append(bfs(seats, start, (-1, 1)))  # NE
+    neighbours.append(bfs(seats, start, (0, -1)))  # W
+    neighbours.append(bfs(seats, start, (0, 1)))   # E
+    neighbours.append(bfs(seats, start, (1, -1)))  # SW
+    neighbours.append(bfs(seats, start, (1, 0)))   # S
+    neighbours.append(bfs(seats, start, (1, 1)))   # SE
 
-    # east
-    x, y = i, j
-    while y < len(seats[0]):
-        if (x == i and y == j) or seats[x][y] == '.':
-            y += 1
-            continue
-        if seats[x][y] != '.':
-            adj_list.append(seats[x][y])
-            break
-        y += 1
-
-    # south west
-    x, y = i, j
-    while x < len(seats) and y >= 0:
-        if (x == i and y == j) or seats[x][y] == '.':
-            x += 1
-            y -= 1
-            continue
-        if seats[x][y] != '.':
-            adj_list.append(seats[x][y])
-            break
-        x += 1
-        y -= 1
-
-    # south 
-    x, y = i, j
-    while x < len(seats):
-        if (x == i and y == j) or seats[x][y] == '.':
-            x += 1
-            continue
-        if seats[x][y] != '.':
-            adj_list.append(seats[x][y])
-            break
-        x += 1
-
-    # south east
-    x, y = i, j
-    while x < len(seats) and y < len(seats[0]):
-        if (x == i and y == j) or seats[x][y] == '.':
-            x += 1
-            y += 1
-            continue
-        if seats[x][y] != '.':
-            adj_list.append(seats[x][y])
-            break
-        x += 1
-        y += 1
-
-    return adj_list
+    return list(filter(lambda x : x is not None, neighbours))
 
 def p2(inp):
     new_copy = deepcopy(inp)
     prev = deepcopy(new_copy)
+    
+    neighbours = {}
+    for i in range(len(inp)):
+        for j in range(len(inp[0])):
+            neighbours[(i, j)] = get_neighbours(new_copy, (i, j))
 
     while True:
         for i in range(len(inp)):
             for j in range(len(inp[0])):
                 if prev[i][j] == '.': continue
-                occupied = check_first_seen(prev, i, j)
-                print(occupied)
+                occupied = [True if prev[x][y] == '#' else False for x, y in neighbours[(i, j)]]
                 if prev[i][j] == 'L' and occupied.count(True) == 0:
                     new_copy[i][j] = '#'
-                elif prev[i][j] == '#' and occupied.count(True) >= 4:
+                elif prev[i][j] == '#' and occupied.count(True) >= 5:
                     new_copy[i][j] = 'L'
-        #print_seats(new_copy)
-        break
         if new_copy == prev:
             break
         prev = deepcopy(new_copy)
     return sum(x.count('#') for x in new_copy)
+
 
 inp = [list(i) for i in get_input()]
 print(p1(inp))
