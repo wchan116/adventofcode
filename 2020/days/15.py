8a732ef7fc743b3f7744df3ebc25e54f50328b21
william <wmchan622@gmail.com>
2020-12-15 19:25:36 +1100

    speedup

--- 2020/days/15.py
+++ 2020/days/15.py
@@ -4,20 +4,20 @@ from utils import get_input
 def play_game(nums, end):
     n = len(nums)
 
-    nums_spoken = {nums[i]: i + 1 for i in range(n - 1)}
+    ns = [0 for _ in range(end)]
+    for i in range(n - 1):
+        ns[nums[i]] = i + 1
 
     next_last = nums[-1]
     for i in range(n - 1, end):
         last = next_last
 
-        if last not in nums_spoken:
-            # nums.append(0)
+        if ns[last] == 0:
             next_last = 0
         else:
-            diff = i + 1 - nums_spoken[last]
-            # nums.append(diff)
+            diff = i + 1 - ns[last]
             next_last = diff
-        nums_spoken[last] = i + 1
+        ns[last] = i + 1
     return last
 
 
