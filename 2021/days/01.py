93532b005a014c80abb4e02ed88a0fe8bd9f9ab9
scwz <wmchan622@gmail.com>
2021-12-01 17:52:35 +1100

    fix up 2021-1

--- 2021/days/01.py
+++ 2021/days/01.py
@@ -2,31 +2,19 @@ from utils import get_input
 
 
 def p1(inp):
-    larger = 0
-    prev = int(inp[0])
-    for line in inp[1:]:
-        num = int(line)
-        if num > prev:
-            larger += 1
-        prev = num
-    return larger
-
-def get_window_sum(arr, start):
-    if start + 3 > len(arr):
-        return 0 
-    return sum(arr[start: start+3])
+    return find_larger(inp, get_window_sum)
 
 def p2(inp):
-    inp = [int(x) for x in inp]
-    larger = 0
-    prev = get_window_sum(inp, 0)
-    for i, line in enumerate(inp[1:]):
-        s = get_window_sum(inp, i)
-        if s > prev:
-            larger += 1
-        prev = s
-    return larger
+    return find_larger(inp, get_window_sum, 3)
 
+def get_window_sum(arr, start, additional):
+    if start + additional > len(arr):
+        return 0 
+    return sum(arr[start: start+additional])
+
+def find_larger(inp, sum_func, additional=1):
+    inp = [int(x) for x in inp]
+    return sum(1 for i in range(1, len(inp)) if sum_func(inp, i, additional) > sum_func(inp, i-1, additional))
 
 inp = get_input()
 print(p1(inp))
