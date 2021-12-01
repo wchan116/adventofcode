2362feb01bea2cb26aa9d70d5eb7106ae4e51d61
scwz <wmchan622@gmail.com>
2021-12-01 17:53:38 +1100

    fix up 2021-1

--- 2021/days/01.py
+++ 2021/days/01.py
@@ -7,14 +7,14 @@ def p1(inp):
 def p2(inp):
     return find_larger(inp, get_window_sum, 3)
 
-def get_window_sum(arr, start, additional):
-    if start + additional > len(arr):
+def get_window_sum(arr, start, window_size):
+    if start + window_size > len(arr):
         return 0 
-    return sum(arr[start: start+additional])
+    return sum(arr[start: start+window_size])
 
-def find_larger(inp, sum_func, additional=1):
+def find_larger(inp, sum_func, window_size=1):
     inp = [int(x) for x in inp]
-    return sum(1 for i in range(1, len(inp)) if sum_func(inp, i, additional) > sum_func(inp, i-1, additional))
+    return sum(1 for i in range(1, len(inp)) if sum_func(inp, i, window_size) > sum_func(inp, i-1, window_size))
 
 inp = get_input()
 print(p1(inp))
