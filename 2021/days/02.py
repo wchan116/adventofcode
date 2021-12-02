685b22477b0916dc53a03a7532dc43e5cde6e796
scwz <wmchan622@gmail.com>
2021-12-02 16:37:35 +1100

    refactor 2021 d2

--- 2021/days/02.py
+++ 2021/days/02.py
@@ -1,23 +1,29 @@
 from utils import get_input
 
-
-def p1(inp):
-    hori, vert= 0, 0
+def calc_pos(inp, commands):
+    hori, vert, aim = 0, 0, 0
     for line in inp:
         direction, steps = line.split(' ')
         steps = int(steps)
-        
-        if direction == 'forward':
-            hori += steps
-        elif direction == 'down':
-            vert += steps
-        elif direction == 'up':
-            vert -= steps
+
+        hori, vert, aim = commands[direction](hori, vert, aim, steps)
     return hori * vert
 
-
+def p1(inp):
+    commands = {
+        'forward': lambda h, v, a, x: (h + x, v, 0),
+        'down': lambda h, v, a, x: (h, v + x, 0),
+        'up': lambda h, v, a, x: (h, v - x, 0)
+    }
+    return calc_pos(inp, commands)
 
 def p2(inp):
+    commands = {
+        'forward': lambda h, v, a, x: (h + x, v + a * x, a),
+        'down': lambda h, v, a, x: (h, v, a + x),
+        'up': lambda h, v, a, x: (h, v, a - x)
+    }
+    return calc_pos(inp, commands)
     hori, vert, aim = 0, 0, 0
     for line in inp:
         direction, steps = line.split(' ')
