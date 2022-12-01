9abe336ab80842c35126d1d4a146346e8251b289
scwz <wmchan622@gmail.com>
2022-12-02 01:02:10 +1100

    refactor 2021 day3

--- 2021/days/03.py
+++ 2021/days/03.py
@@ -9,108 +9,25 @@ def p1(inp):
     lsb = [str(int(not int(x))) for x in msb]
     return int(''.join(msb), 2) * int(''.join(lsb), 2)
 
-def get_rating(grid):
-    while len(grid) > 1:
-        ones = 0
-        zeroes = 0
-        for i in range(len(grid[0])):
-            ones = 0
-            zeroes = 0
-            for j in range(len(grid)):
-                if grid[j][i] == '1':
-                    ones += 1
-                else:
-                    zeroes += 1
-            to_remove = []
-            if ones > zeroes:
-                for x in grid:
-                    if x[i] == '0':
-                        to_remove.append(x)
-            elif zeroes > ones:
-                for x in grid:
-                    if x[i] == '1':
-                        to_remove.append(x)
-            elif ones == zeroes:
-                for x in grid:
-                    if x[i] != '1':
-                        to_remove.append(x)
-
-            for rem in to_remove:
-                grid.remove(rem)
-            if len(grid) <= 1:
-                break
+def get_rating(inp, best_pred):
+    keep = inp
+    for i in range(len(inp[0])):
+        if len(keep) <= 1:
+            break
+        grid = np.matrix([list(x) for x in keep])
+        tp = grid.transpose()
+        row = tp[i].tolist()[0]
+        ones = row.count('1')
+        zeroes = row.count('0')
+        best = best_pred(ones, zeroes)
+        keep = [x for x in keep if x[i] == best]
+    return int(keep[0], 2)
 
 def p2(inp):
-    grid = np.matrix([list(x) for x in inp])
-    tp = grid.transpose()
-    for row in tp:
-        r = row.tolist()[0]
-        ones = r.count('1')
-        zeroes = r.count('0')
-    # grid = [x for x in inp]
-    # while len(grid) > 1:
-    #     ones = 0
-    #     zeroes = 0
-    #     for i in range(len(grid[0])):
-    #         ones = 0
-    #         zeroes = 0
-    #         for j in range(len(grid)):
-    #             if grid[j][i] == '1':
-    #                 ones += 1
-    #             else:
-    #                 zeroes += 1
-    #         to_remove = []
-    #         if ones > zeroes:
-    #             for x in grid:
-    #                 if x[i] == '0':
-    #                     to_remove.append(x)
-    #         elif zeroes > ones:
-    #             for x in grid:
-    #                 if x[i] == '1':
-    #                     to_remove.append(x)
-    #         elif ones == zeroes:
-    #             for x in grid:
-    #                 if x[i] != '1':
-    #                     to_remove.append(x)
+    oxygen_generator_rating = get_rating(inp, lambda x, y: '1' if x >= y else "0")
+    co2_scrubber_rating = get_rating(inp, lambda x, y: '1' if x < y else "0")
 
-    #         for rem in to_remove:
-    #             grid.remove(rem)
-    #         if len(grid) <= 1:
-    #             break
-
-    # oxygen_generator_rating = grid[0]
-    # grid = [x for x in inp]
-    # while len(grid) > 1:
-    #     ones = 0
-    #     zeroes = 0
-    #     for i in range(len(grid[0])):
-    #         ones = 0
-    #         zeroes = 0
-    #         for j in range(len(grid)):
-    #             if grid[j][i] == '1':
-    #                 ones += 1
-    #             else:
-    #                 zeroes += 1
-    #         to_remove = []
-    #         if ones > zeroes:
-    #             for x in grid:
-    #                 if x[i] == '1':
-    #                     to_remove.append(x)
-    #         elif zeroes > ones:
-    #             for x in grid:
-    #                 if x[i] == '0':
-    #                     to_remove.append(x)
-    #         elif ones == zeroes:
-    #             for x in grid:
-    #                 if x[i] != '0':
-    #                     to_remove.append(x)
-
-    #         for rem in to_remove:
-    #             grid.remove(rem)
-    #         if len(grid) <= 1:
-    #             break
-    # co2_scrubber_rating = grid[0]
-    # return int(oxygen_generator_rating, 2) * int(co2_scrubber_rating, 2)
+    return oxygen_generator_rating * co2_scrubber_rating
 
 
 inp = get_input()
